7ba53ba1533732ed4a96250b6f83413ab9f7993f
George Wang <gwang24@gmail.com>
2024-03-11 23:43:53 -0500

    Fix some bugs for instance launch

--- data_prep/download_pile.py
+++ data_prep/download_pile.py
@@ -1,6 +1,15 @@
+import os
+
 from constants import REPO_ID
 from huggingface_hub import snapshot_download
 
-path = snapshot_download(repo_id=REPO_ID, repo_type="dataset", max_workers=16)
+DIR = os.path.dirname(os.path.abspath(__file__))
+LOCAL_DIR = os.path.join(DIR, '/../data/')
+
+path = snapshot_download(repo_id=REPO_ID, 
+                         repo_type="dataset", 
+                         max_workers=16,
+                         local_dir=LOCAL_DIR,
+                         local_dir_use_symlinks=False)
 
 print(path)
